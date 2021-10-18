29d289893581e70f5a7b4a2d4b267afb6402b614
djperrone <60362379+djperrone@users.noreply.github.com>
2021-10-17 20:22:14 -0400

    moved test states into a test folder and made main menu the default startup state

--- SpaceAdventuresGL/src/SpaceAdventures/SpaceAdventuresApp.cpp
+++ SpaceAdventuresGL/src/SpaceAdventures/SpaceAdventuresApp.cpp
@@ -1,11 +1,11 @@
 #include "sapch.h"
 #include "SpaceAdventuresApp.h"
-#include "States/TestState.h"
-#include "States/BatchTest.h"
-#include "States/StencilTest.h"
-#include "States/TextTest.h"
+#include "TestStates/TestState.h"
+#include "TestStates/BatchTest.h"
+#include "TestStates/StencilTest.h"
+#include "TestStates/TextTest.h"
 #include "States/MainMenu.h"
-#include "States/ButtonTest.h"
+#include "TestStates/ButtonTest.h"
 
 
 namespace SpaceAdventures {
@@ -16,12 +16,15 @@ namespace SpaceAdventures {
 	SpaceAdventuresApp::SpaceAdventuresApp(std::string_view title, float width, float height)
 		:Application(title, width, height)
 	{
+		// test states
 		//m_StateMachine.PushState(std::make_unique<TestState>(GetWindow(), m_CameraController));
-		//m_StateMachine->PushState(std::make_unique<MainMenu>(GetWindow(), m_CameraController, m_StateMachine));
 		//m_StateMachine->PushState(std::make_unique<TestState>(GetWindow(), m_CameraController, m_StateMachine));
 		//m_StateMachine->PushState(std::make_unique<TextTest>(GetWindow(), m_CameraController));
 		//m_StateMachine->PushState(std::make_unique<BatchTest>(GetWindow(), m_CameraController, m_StateMachine));
 		//m_StateMachine->PushState(std::make_unique<StencilTest>(GetWindow(), m_CameraController, m_StateMachine));
-		m_StateMachine->PushState(std::make_unique<ButtonTest>(GetWindow(), m_CameraController));
+		//m_StateMachine->PushState(std::make_unique<ButtonTest>(GetWindow(), m_CameraController));
+
+		// Actual Gameplay
+		m_StateMachine->PushState(std::make_unique<MainMenu>(GetWindow(), m_CameraController, m_StateMachine));
 	}
 }
